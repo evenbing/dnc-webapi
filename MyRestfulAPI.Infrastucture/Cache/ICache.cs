5ebcc192679771746550d359ba7f15034db54c73
leibo <252517584@qq.com>
2019-05-17 12:08:17 +0800

    add redis

---
+++ MyRestfulAPI.Infrastucture/Cache/ICache.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRestfulAPI.Infrastucture.Cache
+{
+    public interface ICache
+    {
+        /// <summary>
+        /// 根据Key获取缓存实体数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        T Get<T>(string key);
+
+        /// <summary>
+        /// 根据key获取缓存字符串数据
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        string Get(string key);
+
+        /// <summary>
+        /// 根据key判断缓存是否存在
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        bool GetExists(string key);
+
+        /// <summary>
+        /// 根据Key删除缓存
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        bool Delete(string key);
+
+        /// <summary>
+        /// 设置byte[]类型缓存
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        bool Set(string key, byte[] value);
+
+        /// <summary>
+        /// 设置byte[]类型缓存,并设置缓存过期时间
+        /// </summary>
+        /// <param name="Key"></param>
+        /// <param name="value"></param>
+        /// <param name="ts"></param>
+        /// <returns></returns>
+        bool Set(string Key, byte[] value, TimeSpan ts);
+
+        /// <summary>
+        /// 设置T类型缓存
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        bool Set<T>(string key, T value);
+
+        /// <summary>
+        /// 设置T类型缓存并设置缓存过期时间
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="ts"></param>
+        /// <returns></returns>
+        bool Set<T>(string key, T value, TimeSpan ts);
+
+        /// <summary>
+        /// 设置object类型缓存
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        bool Set<T>(string key, object value);
+
+        /// <summary>
+        /// 设置object类型缓存并设置过期时间
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="ts"></param>
+        /// <returns></returns>
+        bool Set<T>(string key, object value, TimeSpan ts);
+    }
+}
---
+++ MyRestfulAPI.Infrastucture/Cache/RedisCache.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+namespace MyRestfulAPI.Infrastucture.Cache
+{
+    public class RedisCache : ICache
+    {
+        private static string _redisConnectionString;
+
+        private static ConnectionMultiplexer RedisConnection;
+        private static readonly object MLock = new object();
+        private IConfiguration _configuration;
+
+        public RedisCache(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            if (string.IsNullOrEmpty(_redisConnectionString))
+            {
+                var redisConnectionString = _configuration["RedisConnectionString"];
+                if (string.IsNullOrWhiteSpace(redisConnectionString))
+                {
+                    throw new ArgumentException("redis连接字符串未找到");
+                }
+                _redisConnectionString = redisConnectionString;
+            }
+
+        }
+        /// <summary>
+        /// redis 连接对象
+        /// </summary>
+        /// <returns></returns>
+        private ConnectionMultiplexer GetRedisConnection()
+        {
+            if (RedisConnection != null && RedisConnection.IsConnected)
+            {
+                return RedisConnection;
+            }
+
+            lock (MLock)
+            {
+                if (RedisConnection != null && RedisConnection.IsConnected)
+                {
+                    return RedisConnection;
+                }
+                else
+                {
+                    if (RedisConnection !=null)
+                    {
+                        RedisConnection.Dispose();
+                    }
+                    RedisConnection = ConnectionMultiplexer.Connect(_redisConnectionString);
+                }
+            }
+
+            return RedisConnection;
+
+        }
+
+
+        public bool Delete(string key)
+        {
+           return RedisConnection.GetDatabase().KeyDelete(key);
+        }
+
+        public T Get<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetExists(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Set(string key, byte[] value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Set(string Key, byte[] value, TimeSpan ts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Set<T>(string key, T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Set<T>(string key, T value, TimeSpan ts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Set<T>(string key, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Set<T>(string key, object value, TimeSpan ts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
